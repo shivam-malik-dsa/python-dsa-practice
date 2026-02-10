cdf965f40d207c6547dce9fee934e90a29ba34d7
Shivam Malik <shivammalik.work@gmail.com>
2026-02-10 22:44:17 +0530

    implemented next greater element using stack

---
+++ python-dsa/stack/next_greater_element.py
@@ -0,0 +1,14 @@
+def next_greater_element(arr):
+    n=len(arr)
+    stack=[]
+    result= [-1] * n
+
+    for i in range(n):
+        
+        while stack and arr[i] > arr[stack[-1]]:
+            index= stack.pop()
+            result[index]= arr[i]
+        
+        stack.append(i)
+    
+    return result
